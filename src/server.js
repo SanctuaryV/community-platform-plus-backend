7d33fd785a4df17a15b4a5df0f8acfa233cf5d12
SanctuaryV <162547082+SanctuaryV@users.noreply.github.com>
2024-11-27 05:56:25 +0700

    Pass!

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const socketIo = require('socket.io');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth.routes');
-const chat = require('./routes/chat.routes')
+const chat = require('./routes/chat.routes');
 const communityRoutes = require('./routes/community.routes');
 const messageRoutes = require('./routes/messages.routes');
 const profileRoutes = require('./routes/profile.routes');
@@ -14,9 +14,14 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express.json());
 
-app.use('/post', express.static(path.join(__dirname, '..', 'post'))); 
+app.use(express.json());
+app.use(cors({
+    origin: "*",  // รองรับทุก origin
+    methods: ['GET', 'POST', 'PUT'],
+}));
+
+app.use('/post', express.static(path.join(__dirname, '..', 'post')));
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 // สร้าง HTTP server และเชื่อมกับ socket.io
@@ -24,13 +29,12 @@ const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-// สร้างตัว instance ของ socket.io ที่เชื่อมกับ HTTP server และตั้งค่า CORS
 const io = socketIo(server, {
     cors: {
-        origin: "*",  // กำหนดให้รองรับทุก origin หรือกำหนดเฉพาะ IP ที่มือถือใช้
-        methods: ['GET', 'POST', 'PUT'],
+        origin: "*",  // รองรับทุก origin
+        methods: ['GET', 'POST'],
     },
-    transports: ['websocket', 'polling'], // ใช้ transport ที่รองรับได้ดี
+    transports: ['websocket', 'polling'],
 });
 
 app.use(bodyParser.json());
@@ -55,19 +59,13 @@ connection.connect((err) => {
 
 // เชื่อมต่อกับ socket.io
 io.on('connection', (socket) => {
-
-    // เมื่อผู้ใช้เลือกที่จะเข้าร่วมห้อง
     socket.on('joinRoom', (roomId) => {
         socket.join(roomId);
         console.log(`User joined room: ${roomId}`);
         socket.to(roomId).emit('roomJoined', { roomId });
     });
 
-    // เมื่อผู้ใช้ส่งข้อความ
     socket.on('send_message', (messageData) => {
-        console.log(messageData); // ตรวจสอบว่า senderId ถูกส่งมาหรือไม่
-
-        // บันทึกข้อความในฐานข้อมูล (INSERT INTO messages)
         const { roomId, senderId, message } = messageData;
         const query = 'INSERT INTO messages (room_id, sender_id, message) VALUES (?, ?, ?)';
         
@@ -77,15 +75,9 @@ io.on('connection', (socket) => {
                 return;
             }
             console.log('Message inserted into database');
-            
-            // หลังจากบันทึกข้อความแล้ว ส่งข้อความไปยังห้องที่ผู้ใช้เข้าร่วม
             io.to(roomId).emit('receive_message', messageData);
         });
     });
 
-    // เมื่อผู้ใช้หลุดจากห้อง
-    socket.on('disconnect', () => {
-    });
+    socket.on('disconnect', () => {});
 });
-
-
