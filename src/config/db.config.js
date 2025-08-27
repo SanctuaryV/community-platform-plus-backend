f5deaf178b2e748c5ef39c67ad116d4cccdd46e6
SanctuaryV <rebornarm@gmail.com>
2025-08-27 15:02:34 +0700

    commit

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'lmag6s0zwmcswp5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'm7o5775w8sxklp9m',
-    password: 'jvrkixkgcha2jpom',
-    database: 't4en7l6c7ymoemy2',
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'community-platform-sg',
 });
 
 module.exports = connection;
