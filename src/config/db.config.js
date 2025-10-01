87e36edc32cf76ff1a8facfadb288b7e4e3608d3
Danasawin <danasawin-eng@hotmail.com>
2025-10-01 14:22:36 +0700

    adjust dockerfile

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,10 +2,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'community-platform-sg',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'community-platform-sg',
+    port: process.env.DB_PORT || 3306,
 });
 
 module.exports = connection;
